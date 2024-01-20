456a58c6c5264a9742f56100f7bc06539f2c3bbf
slime <85509162+slimeybrooke@users.noreply.github.com>
2024-01-20 01:42:35 -0600

    Update dumbstupiddumb.hx

--- songs/dumbstupiddumb.hx
+++ songs/dumbstupiddumb.hx
@@ -1,5 +1,5 @@
 import funkin.editors.ui.UIState;
 
 function update(elapsed)
-    if (FlxG.keys.justPressed.HOME)
+    if (FlxG.keys.justPressed.F9)
         FlxG.switchState(new UIState(true, 'ToolSelection'));
